36fafceaebe10eb0ff3b5e262c99a19ac0890233
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-05-09 02:51:29 +0100

    first commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants{
+  static String appId = "1:802152565626:web:d6be0759c8da7879b964c3";
+  static String apiKey = "AIzaSyAaasTw3JjQYAk2221XSlftjUVGlvW4iUM";
+  static String messagingSenderId = "802152565626";
+  static String projectId = "chatappflutter-e0701";
+  final primaryColor = const Color(0xFFee7b64);
+}
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const testInputDecoration = InputDecoration(
+  labelStyle: TextStyle(color: Colors.black, fontWeight: FontWeight.w300 ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(
+      color: Color(0xFFee7b64),
+    ),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(
+      color: Color(0xFFee7b64),
+    ),
+  ),
+  errorBorder: OutlineInputBorder(
+    borderSide: BorderSide(
+      color: Color(0xFFee7b64),
+    ),
+  ),
+);
