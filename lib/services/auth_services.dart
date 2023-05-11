a7c0af6ac932aac07b67f5b0039cf336118afa9f
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-05-11 07:19:56 +0100

    commit

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -10,6 +10,10 @@ class AuthService{
   Future registerUserWithEmailandPassword(String fullName, String email, String password) async {
     try {
       User user = (await firebaseAuth.createUserWithEmailAndPassword(email: email, password: password)).user!;
+      if (user != null) {
+        //call our database services to update the user data
+        return true;
+      }
     } on FirebaseAuthException catch (e) {
       print(e);
     }
