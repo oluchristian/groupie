7f690d5386b7eb9c6ce7f73f97db6e31aaa88c0c
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-05-11 07:20:08 +0100

    auth

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,3 +1,4 @@
+import 'package:chatapp_firebase/services/database_services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService{
@@ -12,10 +13,12 @@ class AuthService{
       User user = (await firebaseAuth.createUserWithEmailAndPassword(email: email, password: password)).user!;
       if (user != null) {
         //call our database services to update the user data
+        await DatabaseService(uid: user.uid).updateUserData(fullName, email);
         return true;
       }
     } on FirebaseAuthException catch (e) {
       print(e);
+      return e;
     }
   }
 
